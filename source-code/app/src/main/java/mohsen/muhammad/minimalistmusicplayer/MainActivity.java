872ce37b989cf69eb4992b836d4a54bb8df8c175
Muhammad Abdelhakam <muhammad.abdelhakam@yahoo.com>
2017-06-22 06:56:06 +0200

    Added splash screen.

--- source-code/app/src/main/java/mohsen/muhammad/minimalistmusicplayer/MainActivity.java
+++ source-code/app/src/main/java/mohsen/muhammad/minimalistmusicplayer/MainActivity.java
@@ -1,11 +1,7 @@
 package mohsen.muhammad.minimalistmusicplayer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// update the theme https://developer.android.com/topic/performance/launch-time.html
+		setTheme(R.style.AppTheme);
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity);
 	}
